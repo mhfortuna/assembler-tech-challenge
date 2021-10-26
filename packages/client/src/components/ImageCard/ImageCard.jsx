4dfdf00e751a1b65f655361805f85f005d686612
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-10-26 15:31:42 +0200

    feat: homepage fetch function and imageCard props to render gifs

--- packages/client/src/components/ImageCard/ImageCard.jsx
+++ packages/client/src/components/ImageCard/ImageCard.jsx
@@ -1,5 +1,62 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { PUBLIC } from "../../constants/routes";
 
-export default function ImageCard() {
-  return <div>Image Card</div>;
+import "./ImageCard.scss";
+
+export default function ImageCard({ content }) {
+  // const { _id, url, userId, title } = content;
+  // const { name: categoryName, _id: categoryId } = content.categoryId;
+  const userState = useSelector((state) => state.user);
+  return (
+    <div className="col">
+      <div className="card shadow-sm">
+        <img
+          className="card-img-top img-thumbnail img-max-size"
+          src={content.isGiphy ? content.images.fixed_height.url : content.url}
+          alt="meme"
+        />
+        <title>{content.title}</title>
+
+        <div className="card-body">
+          <h5 className="card-title">{content.title}</h5>
+
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="btn-group">
+              <Link
+                to={
+                  content.isGiphy
+                    ? content.url
+                    : `${PUBLIC.CONTENT}/${content._id}`
+                }
+                className="btn btn-sm btn-outline-secondary"
+              >
+                View
+              </Link>
+              {content.isGiphy === false &&
+                userState.mongoId === content.userId && (
+                  <button
+                    // TODO: action on edit
+                    type="button"
+                    className="btn btn-sm btn-outline-secondary"
+                  >
+                    Edit
+                  </button>
+                )}
+            </div>
+            <small className="text-muted">
+              {content.isGiphy ? (
+                "Content from GIPHY"
+              ) : (
+                <Link to={`${PUBLIC.CATEGORY}/${content.categoryId._id}`}>
+                  {content.categoryId.categoryName}
+                </Link>
+              )}
+            </small>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- packages/client/src/pages/Public/Home/Home.jsx
+++ packages/client/src/pages/Public/Home/Home.jsx
@@ -1,15 +1,32 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { getGifs } from "../../../api/gif-api";
 import ImageList from "../../../components/ImageList/ImageList";
 import Layout from "../../../components/Layout";
+import Spinner from "../../../components/Spinner";
 
 export default function Home() {
+  const [gifData, setGifData] = useState({ data: [], loaded: false });
+
+  const fetchGifData = async () => {
+    try {
+      const { data } = await getGifs(15, 0);
+      setGifData((prev) => ({ loaded: true, data: data.data }));
+    } catch (error) {
+      toast(error.message, { type: "error" });
+    }
+  };
+
+  useEffect(() => {
+    fetchGifData();
+  }, []);
   return (
     <Layout>
       <div className="container">
-        <h3> Some title here </h3>
+        <h2 className="text-center mb-4 fnt-uppercase">Most popular content</h2>
       </div>
-      <ImageList />
+      {gifData.loaded ? <ImageList contentArray={gifData.data} /> : <Spinner />}
     </Layout>
   );
 }
